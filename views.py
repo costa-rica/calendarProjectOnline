357161829942a410a577c08874f0d9815ae2389f
costa-rica <53882857+costa-rica@users.noreply.github.com>
2020-09-20 04:32:20 -0400

    Added html
    Added html pages in templates folder and made some changes to view.py. Still have some linking issues for about and going back to home page

--- views.py
+++ views.py
@@ -6,14 +6,20 @@ app.config["DEBUG"] = True
 @app.route("/")
 @app.route("/index")
 def index():
-    return """home
-    
+    return """
+    <h2>home</h2><br/>
+    This is just to show how we can write html in the python (flask) file
+    <br/><br/>
+    Here is a link to the <a href="calendar"> calendar</a>
+    <!--if you notice the href in the line above doesn't have calendar.html like the html file would-->
+    <br/><br/>
+    Here's something else that is kinda odd: The link between the calendar page doesn't work when running off
+    views.py. However, it does work if we open it straight from the brower. Can you figure out why? Fix?
     """
 
 @app.route("/calendar")
 def calendar():
     return render_template("calendar.html")
-
 
 
 if __name__ == "__main__":
